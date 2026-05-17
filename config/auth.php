a073af9383344f5e7437247fbbd4658d3f9dbdc7
Humam Daa <humamdaa@gmail.com>
2026-05-17 15:31:55 +0300

    new one on auth

--- config/auth.php
+++ config/auth.php
@@ -37,10 +37,17 @@ return [
     */
 
     'guards' => [
+
         'web' => [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'api' => [
+            'driver' => 'sanctum',
+            'provider' => 'users',
+        ],
+
     ],
 
     /*
