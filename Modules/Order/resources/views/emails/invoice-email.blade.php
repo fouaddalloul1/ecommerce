49aabb92ec8c03b1f2a8fe081e42f23af8f30725
kali <kali@kali>
2026-06-21 05:59:23 +0300

    fix_3

--- Modules/Order/resources/views/emails/invoice-email.blade.php
+++ Modules/Order/resources/views/emails/invoice-email.blade.php
@@ -1,14 +1,14 @@
-@component('mail::message')
+<x-mail::message>
 # Invoice for Order #{{ $msg->order->id }}
 
-Hello {{ $msg->order->user->name }},
+Hello {{ $msg->order->user->name ?? 'Customer' }},
 
-Please find attached the invoice for your order #{{ $msg->order->id }}.
+Please find attached the PDF invoice for your order **#{{ $msg->order->id }}**.
 
-**Order Total:** ${{ number_format($msg->order->total, 2) }}
+**Order Total:** ${{ number_format((float) $msg->order->total, 2) }}
 
 Thank you for shopping with us.
 
 Thanks,<br>
 {{ config('app.name') }}
-@endcomponent
+</x-mail::message>
---
+++ storage/framework/views/60708a5f7780326cc8b5c089613177cb.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>: <?php echo e($url); ?>
+
+<?php /**PATH /home/kali/final_ecom/ff/ecommers-batch-queues-fixed/ecommers/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
